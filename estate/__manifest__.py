5066f5bdc8cb9b184fedd632c249c0214a5fd973
Raphael-Carina <rollier@carina.consulting>
2024-10-25 12:32:49 +0200

    Debut projet: création fichier .sh pour lancer le serveur

---
+++ estate/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name' : "Real Estate",
+    'installable': True,
+    'author': "d'Artagnan",
+    'description': """This module is used to create/organize estate properties.""",
+    'depends': ['base'],
+}
---
+++ launch_odoo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+C:\\Users\\Raphaël\\dev\\autoformation\\env\\Scripts\\python.exe C:\\Users\\Raphaël\\dev\\odoo17\\odoo\\odoo-bin -c C:\\Users\\Raphaël\\dev\\autoformation\\odoo.conf --data-dir=C:\\Users\\Raphaël\\dev\\autoformation\\data_dir --addons-path='C:\\Users\\Raphaël\\dev\\odoo17\\odoo\\addons,C:\\Users\\Raphaël\\dev\\odoo17\\odoo\\odoo\\addons,C:\\Users\\Raphaël\\dev\\odoo17\\enterprise,C:\\Users\\Raphaël\\dev\\autoformation' -u estate
