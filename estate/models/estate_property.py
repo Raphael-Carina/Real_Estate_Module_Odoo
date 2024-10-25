eea382cd3841c2cc5b3da68a5792728287feca64
Raphael-Carina <rollier@carina.consulting>
2024-10-25 18:02:52 +0200

    Creation of simples fields for estate.property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,9 +2,29 @@ import sys
 
 sys.path.append('C:\\Users\\Raphaël\\dev\\odoo17\\odoo')
 
-from odoo import models
+from odoo import models, fields
 
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
+    _description = """This model is used to create real estate properties ads"""
+
+    # ==============
+    # Model's fields
+    # ==============
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection=[('south','South'),('west','West'),('north','North'),('east','East')])
+
