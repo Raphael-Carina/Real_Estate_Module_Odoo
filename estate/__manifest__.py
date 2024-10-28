31ebfd2d1f3aa0d33f16efdc5d60b8fd903a7ecd
Raphael-Carina <rollier@carina.consulting>
2024-10-28 10:12:27 +0100

    Ajout du rep security et des droits acces
    Ajout des droits d'accès dans le fichier ir.model.access.csv et chargement de ce fichier dans le manifest.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,4 +4,7 @@
     'author': "d'Artagnan",
     'description': """This module is used to create/organize estate properties.""",
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+        ]
 }
