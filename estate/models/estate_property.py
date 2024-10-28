d29dc3a71b422e53527ec559b6d0f300a0e8803c
Raphael-Carina <rollier@carina.consulting>
2024-10-28 16:47:01 +0100

    Ajout attribut dans les fields du modèle estate.property
    Ajout d'attribut readonly et default + ajout fields 'activate'.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -17,14 +17,27 @@ class EstateProperty(models.Model):
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection=[('south','South'),('west','West'),('north','North'),('east','East')])
+    active = fields.Boolean(default=True)
+    possible_state = [('new','New'),('offer_received','Offer Received'),('offer_acceptred','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')]
+    state = fields.Selection(selection=possible_state, required=True, copy=False, default='new')
+
+    """
+    Explications sur les différents fields et leurs attributs possibles :
+    _____________________________________________________________________
+
+
+    - l'attribut copy=False permet de faire en sorte que le champ en question ne soit pas copier/coller lorsque l'on duplique un enregistrement du modèle.
+    - certains nom de champs sont réservés par Odoo. C'est le cas du nom 'active'. Lorsque ce champ est sur False, l'enregistrement en question disparait de la liste
+    des enregistrements. Il faut modifier les filtres de recherche pour le retrouver.
+    """
 
