3f4b38649083e97afa2572552aafdc9c6d77d377
Raphael-Carina <rollier@carina.consulting>
2024-10-28 12:32:30 +0100

    Ajout de la 1ere action pour mon modèle principal
    Création de la première action de base dans le fichier estate_property_views.py et ajout dans le manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,8 @@
     'description': """This module is used to create/organize estate properties.""",
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'views/estate_property_views.xml',
+
+        'security/ir.model.access.csv',
         ]
 }
