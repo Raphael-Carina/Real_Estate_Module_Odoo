f0938bc0641b62e52d1eafe72ed3e6d6ba02594f
Raphael-Carina <rollier@carina.consulting>
2024-11-04 12:01:57 +0100

    Création de lignes de facture
    On créer 2 lignes de factures automatiques lors de la création de la facture :
    - une ligne avec 6% du prix de vente (selling_price)
    - une ligne avec 100$ de frais administratifs.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import Command, fields, models
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
@@ -11,7 +11,21 @@ class EstateProperty(models.Model):
         # On souhaite générer une facture automatique lorsqu'une annonce est vendue (c'est à dire lorsque l'action action_sold est trigger)
         invoice = self.env['account.move'].create({
             'partner_id' : self.buyer.id,
-            'move_type': 'out_invoice'
+            'move_type': 'out_invoice',
+
+            # Création de ligne de facture (invoice lines)
+            'invoice_line_ids' : [
+                Command.create({
+                    "name": "6% Selling Price",
+                    "quantity": 1,
+                    "price_unit": self.selling_price * 0.06,
+                }),
+                Command.create({
+                    "name": "Administrative fees",
+                    "quantity": 1,
+                    "price_unit": 100
+                }),
+            ],
         })
 
         return super().action_sold()
