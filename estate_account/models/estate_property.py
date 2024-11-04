ebacb7f237f5779710966fdf5e18fc418831f70c
Raphael-Carina <rollier@carina.consulting>
2024-11-04 11:46:58 +0100

    Création d'une facture pour l'acheteur sans ligne

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -6,5 +6,12 @@ class EstateProperty(models.Model):
     The main objective is to add specific comportment of the sold action to automaticaly create an invoice."""
 
     def action_sold(self):
-        print('Méthode action_sold surchargée !')
+        # print('Méthode action_sold surchargée !')
+
+        # On souhaite générer une facture automatique lorsqu'une annonce est vendue (c'est à dire lorsque l'action action_sold est trigger)
+        invoice = self.env['account.move'].create({
+            'partner_id' : self.buyer.id,
+            'move_type': 'out_invoice'
+        })
+
         return super().action_sold()
